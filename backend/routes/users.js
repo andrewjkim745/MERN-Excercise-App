9f8a8c2a394e3d5b443e759a97c48507be5a2b59
Andrew Kim <andrewk745@gmail.com>
2021-12-22 16:02:10 -0500

    hard coded connection string to heroku config var

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -24,32 +24,5 @@ router.route('/add').post((req, res) => {
     .then(() => res.json('User added!'))
     .catch(err => res.status(400).json('Error: ' + err));
 });
-router.post('/register', async (req, res) => {
-    console.log(req.body)
-    try {
-        const user = await User.create({
-            name: req.body.name,
-            email: req.body.email,
-            password: req.body.password
-        })
-        res.json({ status: 'ok' })
-    } catch (err) {
-        res.json({ status: 'error', error: 'Duplicate'})
-    }
-})
-
-router.post('/login', async (req, res) => {
-
-       const user = await User.findOne({ email: req.body, password: req.body.password })
-        res.json({ status: 'ok' })
-        res.json({ status: 'error', error: 'Duplicate'})
-
-        if (user) {
-            return res.json({ status: 'ok', user: true })
-        } else {
-            return res.json({ status:'error', user:false})
-        }
-    })
-
 
 module.exports = router;
