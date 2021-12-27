e2a390d38f3e61c54130d18a116a9e495c3527f1
Andrew Kim <andrewk745@gmail.com>
2021-12-27 01:39:03 -0500

    added getExercises function to exercises component

--- src/Components/Screens/Shared/exercises.js
+++ src/Components/Screens/Shared/exercises.js
@@ -1,12 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 
 
 
 
 export default function Exercises() {
+
+    const [exercises, setExercises ] = useState('')
+    const [ empty , setEmpty ] = useState(false)
+    async function getExercises(event) {
+        event.preventDefault()
+        const response = await fetch(`http://localhost:5000/users${user.id}`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                description, 
+                duration, 
+                date,
+            }),
+        })
+        
+
+        const data = await response.json()
+        console.log(data)
+        if (data) {
+            setExercises(data)
+        } else {
+            setEmpty(true)
+        }
+    }
     return (
         <>
+
         </>
     )
 }
